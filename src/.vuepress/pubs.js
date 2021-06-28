1f47635a0ace208c6d740cc75d3602c60fcb9a20
Xy Ren <xy.r@outlook.com>
2021-06-28 12:47:43 +0800

    Suppress @ice1000's bad typesetting on Publications page

---
+++ src/.vuepress/pubs.js
@@ -0,0 +1,23 @@
+module.exports = {
+  authors: {
+    'Tesla Zhang': 'https://personal.psu.edu/yqz5714'
+  },
+  entries: [
+    {
+      type: 'Papers',
+      items: [
+        {
+          title: 'A simpler encoding of indexed types',
+          conference: 'Proceedings of the 6th ACM SIGPLAN International Workshop on Type-Driven Development (TyDe ’21)',
+          author: 'Tesla Zhang',
+          links: [['arxiv', '2103.15408'], ['doi', '10.1145/3471875.3472991']],
+        },
+        {
+          title: 'Elegant elaboration with function invocation',
+          author: 'Tesla Zhang',
+          links: [['arxiv', '2105.14840']],
+        }
+      ]
+    }
+  ]
+}
