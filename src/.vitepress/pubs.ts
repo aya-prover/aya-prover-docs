60e35eebcf6f2b00eb959fb6a0529154b1e78042
Sepush <sepush@outlook.com>
2022-09-11 03:30:10 +0800

    refactor: support props
    Signed-off-by: Sepush <sepush@outlook.com>

--- src/.vitepress/pubs.ts
+++ src/.vitepress/pubs.ts
@@ -1,33 +1,35 @@
-export const publications = {
-  authors: {
-    'Tesla Zhang': 'https://personal.psu.edu/yqz5714',
+import type { Publicaions } from "./interface"
+export const publications:Publicaions = [
+  {
+    type: 'Papers',
+    items: [
+      {
+        title: 'A simpler encoding of indexed types',
+        venue: 'Proceedings of the 6th ACM SIGPLAN International Workshop on Type-Driven Development (TyDe ’21)',
+        author: {
+          name: 'Tesla Zhang',
+          link: 'http://personal.psu.edu/yqz5714/'
+        },
+        links: [
+          ['arxiv', '2103.15408'],
+          ['doi', '10.1145/3471875.3472991'],
+          ['conference', 'https://personal.psu.edu/yqz5714/gadt-cr.pdf'],
+          ['latest', 'https://personal.psu.edu/yqz5714/gadt.pdf']
+        ],
+      },
+    ],
   },
-  entries: [
-    {
-      type: 'Papers',
-      items: [
-        {
-          title: 'A simpler encoding of indexed types',
-          venue: 'Proceedings of the 6th ACM SIGPLAN International Workshop on Type-Driven Development (TyDe ’21)',
-          author: 'Tesla Zhang',
-          links: [
-            ['arxiv', '2103.15408'],
-            ['doi', '10.1145/3471875.3472991'],
-            ['conference', 'https://personal.psu.edu/yqz5714/gadt-cr.pdf'],
-            ['latest', 'https://personal.psu.edu/yqz5714/gadt.pdf']
-          ],
+  {
+    type: 'Preprints',
+    items: [
+      {
+        title: 'Elegant elaboration with function invocation',
+        author: {
+          name: 'Tesla Zhang',
+          link: 'http://personal.psu.edu/yqz5714/'
         },
-      ],
-    },
-    {
-      type: 'Preprints',
-      items: [
-        {
-          title: 'Elegant elaboration with function invocation',
-          author: 'Tesla Zhang',
-          links: [['arxiv', '2105.14840'], ['latest', 'https://personal.psu.edu/yqz5714/defcall.pdf']],
-        },
-      ],
-    }
-  ],
-}
+        links: [['arxiv', '2105.14840'], ['latest', 'https://personal.psu.edu/yqz5714/defcall.pdf']],
+      },
+    ],
+  }
+]
